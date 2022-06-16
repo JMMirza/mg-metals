c62092904d651ef95061a67c2e2bfff4e7cca61d
JMMirza <jan.muhammad@beaconhousetechnology.com>
2022-06-16 22:00:59 +0500

    changes

--- resources/views/frontend/home/about.blade.php
+++ resources/views/frontend/home/about.blade.php
@@ -1,30 +1,41 @@
 <section class="page-section" id="about">
     <div class="container relative">
-        
+
         <h2 class="section-title font-alt align-left mb-70 mb-sm-40">
-            About Studio
-            
-            <a href="#" class="section-more right">More about us <i class="fa fa-angle-right"></i></a>
-            
+            MG Group was established in 2001
+
+            <a target="_blank" href="https://mggroup.com.hk/?lang=en" class="section-more right"> More about us
+                <i class="fa fa-angle-right"></i></a>
+
         </h2>
-        
+
         <div class="section-text">
             <div class="row">
-                
+
                 <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus ligula semper metus pellentesque mattis. Maecenas  volutpat, diam enim sagittis quam, id porta quam. Sed id dolor consectetur fermentum nibh volutpat, accumsan purus. 
+
+                    MG Metals is part of the group's core business which include a variety of financial services, real
+                    estate, entertainment, publishing, arts and cultures, education and training, multimedia and
+                    environmental protection. Not only has the scale of our business across China, but also the world.
+
                 </div>
-                
+
                 <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus ligula semper metus pellentesque mattis. Maecenas  volutpat, diam enim sagittis quam, id porta quam. Sed id dolor consectetur fermentum nibh volutpat, accumsan purus. 
+                    For financial business, MG is one of the minor companies that can provide comprehensive financial
+                    products such as gold trading, securities, futures, and derivatives trading investment, investment
+                    immigration, and credit services in Hong Kong.
+
                 </div>
-                
+
                 <div class="col-md-4 col-sm-6 mb-sm-50 mb-xs-30">
-                    Etiam sit amet fringilla lacus. Pellentesque suscippiteme ante at ullamcorper pulvinar neque porttitor. Integer lectus. Praesent sed nisi eleifend, fermentum orci amet, iaculis libero. Donec vel ultricies purus. Nam dictum sem, eu aliquam.
+                    The success of our company stems from its adherence to integrity, professionalism and excellence.
+                    The company strives to expand the various core business with the most outstanding products and
+                    services so the customers can seize every opportunity create a better future.
+
                 </div>
-                
+
             </div>
         </div>
-        
+
     </div>
 </section>
--- resources/views/frontend/home/testimonials.blade.php
+++ resources/views/frontend/home/testimonials.blade.php
@@ -1,5 +1,6 @@
-<section class="page-section bg-dark bg-dark-alfa-90 fullwidth-slider" data-background="{{ asset('frontend/images/full-width-images/section-bg-3.jpg') }}">
-        
+<section class="page-section bg-dark bg-dark-alfa-90 fullwidth-slider"
+    data-background="{{ asset('frontend/images/full-width-images/section-bg-3.jpg') }}">
+
     <!-- Slide Item -->
     <div>
         <div class="container relative">
@@ -9,7 +10,34 @@
                     <div class="section-icon">
                         <span class="icon-quote"></span>
                     </div>
-                    <!-- Section Title --><h3 class="small-title font-alt">What people say?</h3>
+                    <!-- Section Title -->
+                    <h3 class="small-title font-alt">What people say?</h3>
+                    <blockquote class="testimonial white">
+                        <p>
+                            Buying and selling gold has never been easier from this trusted brand. Excellent service
+                            rendered with quick response.
+                        </p>
+                        <footer class="testimonial-author">
+                            RICHARD, CUSTOMER
+                        </footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Slide Item -->
+
+    <!-- Slide Item -->
+    <div>
+        <div class="container relative">
+            <div class="row">
+                <div class="col-lg-10 offset-lg-1 col-xl-8 offset-xl-2 align-center">
+                    <!-- Section Icon -->
+                    <div class="section-icon">
+                        <span class="icon-quote"></span>
+                    </div>
+                    <!-- Section Title -->
+                    <h3 class="small-title font-alt">What people say?</h3>
                     <blockquote class="testimonial white">
                         <p>
                             Phasellus luctus commodo ullamcorper a posuere rhoncus commodo elit. Aenean congue,
@@ -24,7 +52,7 @@
         </div>
     </div>
     <!-- End Slide Item -->
-    
+
     <!-- Slide Item -->
     <div>
         <div class="container relative">
@@ -34,7 +62,8 @@
                     <div class="section-icon">
                         <span class="icon-quote"></span>
                     </div>
-                    <!-- Section Title --><h3 class="small-title font-alt">What people say?</h3>
+                    <!-- Section Title -->
+                    <h3 class="small-title font-alt">What people say?</h3>
                     <blockquote class="testimonial white">
                         <p>
                             Phasellus luctus commodo ullamcorper a posuere rhoncus commodo elit. Aenean congue,
@@ -48,29 +77,4 @@
             </div>
         </div>
     </div>
-    <!-- End Slide Item -->
-    
-    <!-- Slide Item -->
-    <div>
-        <div class="container relative">
-            <div class="row">
-                <div class="col-lg-10 offset-lg-1 col-xl-8 offset-xl-2 align-center">
-                    <!-- Section Icon -->
-                    <div class="section-icon">
-                        <span class="icon-quote"></span>
-                    </div>
-                    <!-- Section Title --><h3 class="small-title font-alt">What people say?</h3>
-                    <blockquote class="testimonial white">
-                        <p>
-                            Phasellus luctus commodo ullamcorper a posuere rhoncus commodo elit. Aenean congue,
-                            risus utaliquam dapibus. Thanks!
-                        </p>
-                        <footer class="testimonial-author">
-                            John Doe, doodle inc.
-                        </footer>
-                    </blockquote>
-                </div>
-            </div>
-        </div>
-    </div>    
 </section>
