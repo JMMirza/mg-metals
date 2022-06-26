d41fac72c9d2239abc2cdfdc700a8761729cc863
9meAhmed <noman.ahmed1408@gmail.com>
2022-06-26 10:56:11 -0500

    change

--- resources/views/frontend/profile/shareholders/shareholder_info.blade.php
+++ resources/views/frontend/profile/shareholders/shareholder_info.blade.php
@@ -95,7 +95,7 @@
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="5">No Record Found!</td>
+                        <td colspan="6">No Record Found!</td>
                     </tr>
                 @endforelse
             </tbody>
