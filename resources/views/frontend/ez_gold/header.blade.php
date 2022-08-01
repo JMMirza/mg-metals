9043014e58a4df3e2f1f2b8de3588b29f101f2ab
amirbaig87 <amirbaig87@outlook.com>
2022-08-01 15:50:12 +0500

    ezgold app page

--- resources/views/frontend/ez_gold/header.blade.php
+++ resources/views/frontend/ez_gold/header.blade.php
@@ -1,20 +1,22 @@
-<section class="small-section bg-dark-lighter" style="padding:140px 0px 100px !important" data-background="{{ asset('frontend/images/ezg.png') }}">
+<section class="small-section bg-dark-lighter" style="padding:140px 0px 100px !important;    background-position-y: -30px;
+    background-size: contain;" data-background="{{ asset('frontend/images/ezg.png') }}">
     <div class="relative container align-left">
 
         <div class="row">
 
             <div class="col-md-8">
-                <h1 class="hs-line-11 font-alt mb-20 mb-xs-0">{{ __('home_page.ez_gold') }}</h1>
-                <div class="hs-line-4 font-alt">
+                <img src="{{ asset('frontend/images/app-logo.png') }}" class="mb-3" alt="logo">
+                <div class="hs-line-4 font-alt ">
                 ONE APP, MANY USES.
                 </div> 
+               
             </div>
 
             <div class="col-md-4 mt-30">
-                <div class="mod-breadcrumbs font-alt align-right">
+                <!-- <div class="mod-breadcrumbs font-alt align-right">
                     <a
                         href="{{ route('home') }}">{{ __('home_page.home') }}</a>&nbsp;/&nbsp;<span>{{ __('home_page.ez_gold') }}</span>
-                </div>
+                </div> -->
             </div>
         </div>
 
--- resources/views/frontend/ez_gold/index.blade.php
+++ resources/views/frontend/ez_gold/index.blade.php
@@ -8,7 +8,7 @@
 
             <div class="row">
 
-                <div class="col-md-6  mb-sm-40">
+                <div class="col-md-7 align-self-center  mb-sm-40">
 
                     <!-- About Project -->
                     <div class="text">
@@ -20,8 +20,12 @@ Whenever & Where you want.</h3>
                         </p>
 
                         <div class="mt-40">
-                            <a href="#" class="btn btn-mod btn-border btn-round btn-medium" target="_blank">View
-                                online</a>
+                            <a href="#" target="_blank">
+                                <img src="{{ asset('frontend/images/app-store.png') }}" class="mr-0" style="width:180px;margin-right:30px" alt="logo">
+                            </a>
+                            <a href="#" target="_blank">
+                                <img src="{{ asset('frontend/images/play-store.png') }}" class="mr-0" style="width:180px"  alt="logo">
+                            </a>
                         </div>
 
                     </div>
@@ -29,18 +33,22 @@ Whenever & Where you want.</h3>
 
                 </div>
 
-                <div class="col-md-6">
+                <div class="col-md-5">
 
                     <!-- Work Gallery -->
                     <div class="work-full-media mt-0 white-shadow">
-                        <ul class="clearlist work-full-slider owl-carousel">
+                        <ul class="clearlist work-full-slider owl-carousel mobile-app">
                             <li>
-                                <img class="lazyOwl" src="{{ asset('/frontend/images/portfolio/projects-thumb.gif') }}"
-                                    data-src="{{ asset('frontend/images/portfolio/full-project-3.jpg') }}" alt="" />
+                                <img class="lazyOwl" src="{{ asset('/frontend/images/apppic1.png') }}"
+                                    data-src="{{ asset('frontend/images/apppic1.png') }}" alt="" />
                             </li>
                             <li>
-                                <img class="lazyOwl" src="{{ asset('frontend/images/portfolio/projects-thumb.gif') }}"
-                                    data-src="{{ asset('frontend/images/portfolio/full-project-4.jpg') }}" alt="" />
+                                <img class="lazyOwl" src="{{ asset('frontend/images/apppic2.png') }}"
+                                    data-src="{{ asset('frontend/images/apppic2.png') }}" alt="" />
+                            </li>
+                            <li>
+                                <img class="lazyOwl" src="{{ asset('frontend/images/apppic3.png') }}"
+                                    data-src="{{ asset('frontend/images/apppic3.png') }}" alt="" />
                             </li>
                         </ul>
                     </div>
@@ -55,60 +63,55 @@ Whenever & Where you want.</h3>
 
     {{-- <hr class="mt-0 mb-0" /> --}}
 
-    <section class="page-section bg-dark-alfa-70"
-        data-background="{{ asset('frontend/images/full-width-images/section-bg-16.jpg') }}">
+    <section class="page-section "
+        data-background="{{ asset('frontend/images/why-banner.png') }}">
         <div class="container relative">
 
-            <h2 class="section-title font-alt mb-70 mb-sm-40">
-                Why Choose Us?
+            <h2 class="section-title font-alt playfare dark mb-10 text-center">
+                What can EZgold App do?
             </h2>
+            <p class="dark mb-3 text-center">Join over a million happy users!</p>
 
             <!-- Features Grid -->
-            <div class="row multi-columns-row alt-features-grid">
+            <div class="row multi-columns-row alt-features-grid mt-5">
 
+                
                 <!-- Features Item -->
-                <div class="col-sm-6 col-md-4 col-lg-4">
+                <div class="col-sm-6 col-md-3">
                     <div class="alt-features-item align-center">
-                        <div class="alt-features-icon">
-                            <span class="icon-flag"></span>
-                        </div>
-                        <h3 class="alt-features-title font-alt">We’re Creative</h3>
+                        <h3 class="alt-features-title font-alt playfare">Purchase</h3>
                         <div class="alt-features-descr align-left">
-                            Lorem ipsum dolor sit amet, c-r adipiscing elit.
-                            In maximus ligula semper metus pellentesque mattis.
-                            Maecenas volutpat, diam enim.
+                        Purchases are made easy through your EZgold wallet.
                         </div>
                     </div>
                 </div>
                 <!-- End Features Item -->
-
                 <!-- Features Item -->
-                <div class="col-sm-6 col-md-4 col-lg-4">
+                <div class="col-sm-6 col-md-3">
                     <div class="alt-features-item align-center">
-                        <div class="alt-features-icon">
-                            <span class="icon-clock"></span>
-                        </div>
-                        <h3 class="alt-features-title font-alt">We’re Punctual</h3>
+                        <h3 class="alt-features-title font-alt playfare">Cash-Out</h3>
                         <div class="alt-features-descr align-left">
-                            Proin fringilla augue at maximus vestibulum.
-                            Nam pulvinar vitae neque et porttitor. Praesent sed
-                            nisi eleifend, lorem fermentum orci sit amet, iaculis libero.
+                        Sell your gold through EZgold App & receive cash in your EZgold wallet.
                         </div>
                     </div>
                 </div>
                 <!-- End Features Item -->
-
                 <!-- Features Item -->
-                <div class="col-sm-6 col-md-4 col-lg-4">
+                <div class="col-sm-6 col-md-3">
                     <div class="alt-features-item align-center">
-                        <div class="alt-features-icon">
-                            <span class="icon-hotairballoon"></span>
-                        </div>
-                        <h3 class="alt-features-title font-alt">We have magic</h3>
+                        <h3 class="alt-features-title font-alt playfare">Transfer</h3>
                         <div class="alt-features-descr align-left">
-                            Curabitur iaculis accumsan augue, nec finibus mauris pretium eu.
-                            Duis placerat ex gravida nibh tristique porta. Nulla facilisi.
-                            Suspendisse ultricies eros blandit.
+                        Transfer gold reserves to another EZgold user.
+                        </div>
+                    </div>
+                </div>
+                <!-- End Features Item -->
+                <!-- Features Item -->
+                <div class="col-sm-6 col-md-3">
+                    <div class="alt-features-item align-center">
+                        <h3 class="alt-features-title font-alt playfare">Extract</h3>
+                        <div class="alt-features-descr align-left">
+                        A number of exquisite solid gold products for you to choose from.
                         </div>
                     </div>
                 </div>
@@ -127,43 +130,32 @@ Whenever & Where you want.</h3>
 
                 <div class="row">
 
-                    <div class="col-md-7">
+                    <div class="col-md-7 align-self-center col-12">
 
-                        <!-- Gallery -->
-                        <div class="work-full-media mt-0 white-shadow">
-                            <ul class="clearlist work-full-slider owl-carousel">
-                                <li>
-                                    <img class="lazyOwl" src="{{ asset('frontend/images/portfolio/projects-thumb.gif') }}"
-                                        data-src="{{ asset('frontend/images/portfolio/full-project-1.jpg') }}"
-                                        alt="" />
-                                </li>
-                                <li>
-                                    <img class="lazyOwl" src="{{ asset('frontend/images/portfolio/projects-thumb.gif') }}"
-                                        data-src="{{ asset('frontend/images/portfolio/full-project-2.jpg') }}"
-                                        alt="" />
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- End Gallery -->
+                    <img src="{{ asset('frontend/images/person-withapp.png') }}" alt="" />
 
                     </div>
 
-                    <div class="col-md-5 col-lg-4 offset-lg-1">
+                    <div class="col-md-5 col-lg-4 offset-lg-1 align-self-center">
 
                         <div class="row">
 
-                            <div class="col-md-12">
-                                <h2 style="font-size:30px; line-height:normal;"><span>Download the app now!</span></h2>
-                                <img src="{{ asset('frontend/images/portfolio/full-project-1.jpg') }}" alt="" />
-                                <h2 class="font_3" style="font-size:22px; line-height:normal;"><span
-                                        style="font-size:22px;">Join EZgold&nbsp;</span><span
-                                        style="font-size:24px;">community</span></h2>
+                            <div class="col-md-12 text-center">
+                                <h2 class="playfare dark bold" style="font-size:20px; line-height:normal;"><span>Download the app now!</span></h2>
+                                <img src="{{ asset('frontend/images/download-scan.png') }}" alt="" />
+                                <h3 class="playfare dark" style="font-size:16px;margin-top:10px;">Join EZgold community</h3>
                             </div>
-                            <div class="col-md-6">
-                                <img src="{{ asset('frontend/images/portfolio/full-project-1.jpg') }}" alt="" />
+                            <div class="col-md-4 col-12 text-center mt-3">
+                                <img src="{{ asset('frontend/images/whatsapp-scan.png') }}" alt="" />
+                                <h3 class="playfare gold" style="font-size:14px;">Whatsapp</h3>
                             </div>
-                            <div class="col-md-6">
-                                <img src="{{ asset('frontend/images/portfolio/full-project-1.jpg') }}" alt="" />
+                            <div class="col-md-4 col-12 text-center mt-3">
+                                <img src="{{ asset('frontend/images/wechat-scan.png') }}" alt="" />
+                                <h3 class="playfare gold" style="font-size:14px;">We Chat</h3>
+                            </div>
+                            <div class="col-md-4 col-12 text-center mt-3">
+                                <img src="{{ asset('frontend/images/line-scan.png') }}" alt="" />
+                                <h3 class="playfare gold" style="font-size:14px;">Line</h3>
                             </div>
                         </div>
 
